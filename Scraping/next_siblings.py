0217608d0113c40dca078f33b1da7ce9f74dd38c
Eyakub <eyakubsorkar@gmail.com>
2020-01-06 12:56:43 +0600

    siblings, next and previous siblings

---
+++ Scraping/next_siblings.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+
+def read_file():
+    file = open('sample.html')
+    data = file.read()
+    file.close()
+    return data
+
+soup = BeautifulSoup(read_file(), 'lxml')
+
+body = soup.body
+p = soup.body.p
+
+# body - contents
+# print(body.contents)
+
+# .next_sibling
+print(p.next_sibling.next_sibling)
