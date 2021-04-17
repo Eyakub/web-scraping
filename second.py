6d156c73560309b342850c23917b428f86cb3f93
eyakub <eyakubsorkar@gmail.com>
2021-04-18 02:37:24 +0600

    click(), back(), forward()

---
+++ second.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://techwithtim.net')
+
+course_link = driver.find_element_by_link_text("Python Programming")
+course_link.click()
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+    )
+    element.click()
+
+    driver.back()
+    driver.back()
+    driver.back()
+finally:
+    driver.quit()
