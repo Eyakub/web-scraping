551d3d4bd4247426986315e3f58958761eebe8b3
eyakub <eyakubsorkar@gmail.com>
2021-04-18 02:24:43 +0600

    find element by tag name class name id

---
+++ first.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://techwithtim.net')
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    articles = main.find_elements_by_tag_name('article')
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
